06992a2ab9bf376c7c00d18b0134bd8fc3c44bc8
jspero77 <80274538+jspero77@users.noreply.github.com>
2025-09-22 12:11:18 -0400

    Add project files.

---
+++ Assets/ProGen.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class ProGen : MonoBehaviour
+{
+
+    public int width;
+    public GameObject dirt;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Gen();
+    }
+
+    // Update is called once per frame
+    void Gen()
+    {
+        for (int x = 0; x < width; x++)
+        {
+
+        }
+    }
+}
