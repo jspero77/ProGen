41677a4f257f6e4bde52fd887657f9947a976ca1
jspero77 <80274538+jspero77@users.noreply.github.com>
2025-09-28 17:36:20 -0400

    Procedural Generation Function Works

--- Assets/ProGen.cs
+++ Assets/ProGen.cs
@@ -4,7 +4,8 @@ public class ProGen : MonoBehaviour
 {
 
     public int width;
-    public GameObject dirt;
+    public GameObject dirt,grass;
+    public int height;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -16,7 +17,23 @@ public class ProGen : MonoBehaviour
     {
         for (int x = 0; x < width; x++)
         {
+            int minHeight = height - 1;
+            int maxHeight = height + 2;
 
+            height = Random.Range(minHeight, maxHeight);
+            for (int y = 0; y < height; y++)
+            {
+                
+                spawnObject(dirt, x, y);
+            }
+            spawnObject(grass, x, height);
         }
+ 
+    }
+
+    void spawnObject(GameObject obj, int width, int height)
+    {
+obj = Instantiate(obj, new Vector2(width, height), Quaternion.identity);
+        obj.transform.parent = this.transform;
     }
 }
